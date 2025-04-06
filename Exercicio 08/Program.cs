f7ba025598298db8086ce56c53c51819cc897f22
Filipe <Filipecasagrande1@gmail.com>
2025-04-06 20:06:26 -0300

    exercicio 08

---
+++ Exercicio 08/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Exercicio_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3, resultado;
+            
+            Console.WriteLine($"Informe valor de A");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Informe o valor de B");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Informe o valor de C");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            resultado = num1 + num2;
+
+            if(resultado < num3)
+            {
+                Console.WriteLine("os valores A + B sao menor que C");
+            }
+            else
+            {
+                Console.WriteLine("O valor de C e maio que A + B juntos");
+            }
+        }
+    }
+}
--- Exercicio07/Program.cs
+++ Exercicio07/Program.cs
@@ -5,8 +5,30 @@
         static void Main(string[] args)
         {
             int numero;
-            Console.WriteLine("informe o número para testar se ele é um número primo");
-            numero =Convert.ToInt32(Console.ReadLine());
+            double raiz = 0;
+            Console.WriteLine("Informe o número para ver se ele é um número primo");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero <= 1)
+            {
+                Console.WriteLine($"O número {numero} não é um número primo");
+            }
+
+            for (int i = 2; i < numero; i++)
+            {
+                if (raiz % i != 0)
+                {
+
+                    Console.WriteLine("O número não é primo");
+                }
+
+                else
+                {
+                    Console.WriteLine($"O número é primo");
+                }
+
+                Console.ReadLine();
+            }
 
         }
     }
