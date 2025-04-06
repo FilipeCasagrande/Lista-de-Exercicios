8076be6c013f720b28da1e970cd217f41aa63d2a
Filipe <Filipecasagrande1@gmail.com>
2025-04-06 17:37:42 -0300

    Add project files.

---
+++ Exercicio07/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Exercicio07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.WriteLine("informe o número para testar se ele é um número primo");
+            numero =Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
